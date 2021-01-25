afaab51aea31209e7483232e505ed82c53668a69
markkkkas <klapovsciuk@gmail.com>
2021-01-25 07:47:58 +0200

    Update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,82 +1,83 @@
 module.exports = {
-  title: 'RealState Wiki',
-  tagline: 'Projekto sistemų aprašymai',
-  url: 'https://forumas.realstate.lt',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'RealState', // Usually your GitHub org/user name.
-  projectName: 'Wiki', // Usually your repo name.
+  title: "RealState Wiki",
+  tagline: "Projekto sistemų aprašymai",
+  url: "https://forumas.realstate.lt",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "RealState", // Usually your GitHub org/user name.
+  projectName: "Wiki", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'RealState Wiki',
+      title: "RealState Wiki",
       logo: {
-        alt: 'Wiki logo',
-        src: 'img/logo.svg',
+        alt: "Wiki logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Dokumentacija',
-          position: 'left',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Dokumentacija",
+          position: "left",
         },
-        {to: 'blog', label: 'Blogas', position: 'left'},
+        { to: "blog", label: "Blogas", position: "left" },
         {
-          href: 'https://forumas.realstate.lt/',
-          label: 'RealState Forumas',
-          position: 'right',
+          href: "https://forumas.realstate.lt/",
+          label: "RealState Forumas",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Wiki',
+          title: "Wiki",
           items: [
             {
-              label: 'Pildymo instrukcija',
-              to: 'docs/',
+              label: "Pildymo instrukcija",
+              to: "docs/",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/realstate-lt/wiki',
+              label: "GitHub",
+              href: "https://github.com/realstate-lt/wiki",
             },
           ],
         },
         {
-          title: 'Projektas',
+          title: "Projektas",
           items: [
             {
-              label: 'Serveris',
-              href: 'samp://samp.realstate.lt:7777/',
+              label: "Serveris",
+              href: "samp://samp.realstate.lt:7777/",
             },
             {
-              label: 'Discord',
-              href: 'https://discord.gg/CpMkRxC3sF',
+              label: "Discord",
+              href: "https://discord.realstate.lt",
             },
             {
-              label: 'Forumas',
-              href: 'https://forumas.realstate.lt/',
+              label: "Forumas",
+              href: "https://forumas.realstate.lt/",
             },
           ],
         },
         {
-          title: 'Kita',
+          title: "Kita",
           items: [
             {
-              label: 'VVP',
-              href: 'https://vvp.realstate.lt/login',
+              label: "VVP",
+              href: "https://vvp.realstate.lt/login",
             },
             {
-              label: 'Atnaujinimai',
-              href: 'https://forumas.realstate.lt/forum/7-atnaujinimai/',
+              label: "Atnaujinimai",
+              href: "https://forumas.realstate.lt/forum/7-atnaujinimai/",
             },
             {
-              label: 'Supergames tema',
-              href: 'https://www.supergames.lt/topic/326203-realstatelt-nemokami-prizai-kas-24val/page/1/',
+              label: "Supergames tema",
+              href:
+                "https://www.supergames.lt/topic/326203-realstatelt-nemokami-prizai-kas-24val/page/1/",
             },
           ],
         },
@@ -86,20 +87,18 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/realstate-lt/wiki/tree/master/website',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/realstate-lt/wiki/tree/master",
         },
         blog: {
           showReadingTime: true,
-          editUrl:
-            'https://github.com/realstate-lt/wiki/tree/master/website',
+          editUrl: "https://github.com/realstate-lt/wiki/tree/master",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
