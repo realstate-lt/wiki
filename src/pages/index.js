54e1bf9acf45229a846e3e0739c54f4be267e1bc
tomasvosylius <vsls.tomas@gmail.com>
2021-01-30 23:39:48 +0200

    add background video

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,36 +57,37 @@ function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="RealState projekto Wiki dokumentacijos saugykla">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Skaityti
-            </Link>
-          </div>
-        </div>
-      </header>
+      description="RealState projekto Wiki dokumentacijos saugykla"
+    >
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
+        <div className={styles.videoBackground}>
+          <iframe frameborder="0" height="100%" width="100%"
+            src="https://youtube.com/embed/x6a6DeLVMJU?autoplay=1&controls=0&showinfo=0&autohide=1&autoplay=1">
+          </iframe>
+        </div>
+
+        <div className={styles.content}>
+          <div className={styles.headline}>
+            <h1>{siteConfig.title}</h1>
+            <p>{siteConfig.tagline}</p>
+          </div>
+          
+          
+          
+          {features && features.length > 0 && (
+            <section className={styles.features}>
+              {features.map((props, idx) => (
+                <Feature key={idx} {...props} />
+              ))}
+            </section>
+          )}
+          
+          
+
+        </div>
       </main>
+
+  
     </Layout>
   );
 }
